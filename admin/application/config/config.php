7db33c37a96c372d6bba2683ce34eedc1fa3556d
jishnu <j4mejishnu@yahoo.com>
2021-11-04 13:59:28 +0530

    Localhost file configurations correction

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
  // $config['base_url'] = 'http://127.0.0.1/greenlight_new/admin/';
- $config['base_url'] = 'http://woztiinfotech.com/greendemo/admin/';
+ // $config['base_url'] = 'http://woztiinfotech.com/greendemo/admin/';
+ $config['base_url'] = 'http://localhost/greenlight/admin/';
 //$config['base_url'] = 'http://127.0.0.1/greenlight/Greenlight/';
 
 /*
