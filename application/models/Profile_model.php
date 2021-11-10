74aa2b85a02216f7b2bc8850377eeaf5ec0bf84d
Rajeev Raghavan <rajeev7000raghavan@gmail.com>
2021-11-10 15:02:01 +0530

    changes made to profile pic

--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -1,33 +1,60 @@
-<?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Profile_model extends CI_Model
-{
-
-	public function getUserTableDetails($user_id)
-	{
-
-    // $this->db->select('users.id, users.first_name, users.last_name, users.phone_no, users.email, user_profile_img.user_pimage_name')
-    // ->join('user_profile_img','user_profile_img.user_id_fk = users.id');
-    // $this->db->from('users');
-    // $this->db->where('id',$user_id);
-    // $query = $this->db->get();
-    // return $query->result();
-    $this->db->select('*');
-    $this->db->from('users');
-    $this->db->where('id',$user_id);
-    $query = $this->db->get();
-    return $query->result();
-	}
-
-
-    public function getUserImageTableDetails($user_id)
-	{
-
-    $this->db->select('*');
-    $this->db->from('user_profile_img');
-    $this->db->where('user_id_fk',$user_id);
-    $query = $this->db->get();
-    return $query->result();
-	}
-
-}
+<?php
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Profile_model extends CI_Model
+
+{
+
+
+
+	public function getUserTableDetails($user_id)
+
+	{
+
+
+
+    // $this->db->select('users.id, users.first_name, users.last_name, users.phone_no, users.email, user_profile_img.user_pimage_name')
+
+    // ->join('user_profile_img','user_profile_img.user_id_fk = users.id');
+
+    // $this->db->from('users');
+
+    // $this->db->where('id',$user_id);
+
+    // $query = $this->db->get();
+
+    // return $query->result();
+
+    $this->db->select('*');
+
+    $this->db->from('users');
+
+    $this->db->where('id',$user_id);
+
+    $query = $this->db->get();
+
+    return $query->result();
+
+	}
+
+
+
+
+
+    public function getUserImageTableDetails($user_id)
+
+	{
+
+    $this->db->select('*');
+    $this->db->from('user_profile_img');
+    $this->db->where('user_id_fk',$user_id);
+    $query = $this->db->get();
+    return $query->result();
+
+	}
+
+
+
+}
+
