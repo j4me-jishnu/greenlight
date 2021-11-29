d2f473bf3e83b7872f1aff35e5b92890596febed
jishnu <j4mejishnu@yahoo.com>
2021-11-29 14:29:04 +0530

    Showd QR Code in Admin section Events list

--- admin/application/views/Administration/Events/list.php
+++ admin/application/views/Administration/Events/list.php
@@ -1,45 +1,47 @@
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1 class="row">
-        <!-- Company Management -->
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Events</li>
-      </ol>
-    </section><br>
-
-    <!-- Main content -->
-    <section class="content">
-      <input type="hidden" id="response" value="<?php echo $this->session->flashdata('response');?>" />
-      <div class="box">
-        <div class="box-header">
-          <div class="row">
-            <div class="col-sm-10">
-              <h3>Events</h3>
-            </div>
-            <div class="col-sm-2">
-              <a href="<?php echo base_url();?>addEvent" class="btn btn-sm btn-success"><i class="fa fa-plus-square"></i> Add New Event</a>
-            </div>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1 class="row">
+      <!-- Company Management -->
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Events</li>
+    </ol>
+  </section><br>
+
+  <!-- Main content -->
+  <section class="content">
+    <input type="hidden" id="response" value="<?php echo $this->session->flashdata('response');?>" />
+    <div class="box">
+      <div class="box-header">
+        <div class="row">
+          <div class="col-sm-10">
+            <h3>Events</h3>
+          </div>
+          <div class="col-sm-2">
+            <a href="<?php echo base_url();?>addEvent" class="btn btn-sm btn-success"><i class="fa fa-plus-square"></i> Add New Event</a>
           </div>
         </div>
-        <div class="box-body">
-            <table id="event" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>Sl No.</th>
-                  <th>Event name</th>
-                  <th>Description</th>
-                  <th>Background</th>
-                  <th><center>EDIT/DELETE</center></th>
-                  <th><center>GENERATE BARCODE</center></th>
-                </tr>
-              </thead>
-              <tbody>
-              </tbody>
-            </table>
-        </div>
       </div>
-    </section>
+      <div class="box-body">
+        <table id="event" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>Sl No.</th>
+              <th>Event name</th>
+              <th>Description</th>
+              <th>Background</th>
+              <th><center>EDIT/DELETE</center></th>
+              <th><center>GENERATE BARCODE</center></th>
+              <th><center>QR Code</center></th>
+
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
 </div>
--- admin/application/views/Administration/Events/script.php
+++ admin/application/views/Administration/Events/script.php
@@ -1,60 +1,69 @@
 <script>
-
-  $table = $('#event').DataTable( {
-          "processing": true,
-          "serverSide": true,
-          "bDestroy" : true,
-          "ajax": {
-              "url": "<?php echo base_url();?>Administration/getEvent/",
-              "type": "POST",
-              "data" : function (data) {
-
-             }
-          },
-          "createdRow": function ( row, data, index ){
-            $table.column(0).nodes().each(function(node,index,dt){
-            $table.cell(node).data(index+1);
-            });
-            $('td', row).eq(4).html('<center><a href="<?php echo base_url();?>Administration/editEvent/'+data['event_id']+'"><i class="fa fa-edit iconFontSize-medium" ></i></a>&nbsp;&nbsp;&nbsp;<a onclick="return confirmDelete('+data['event_id']+')"><i class="fa fa-trash-o iconFontSize-medium" ></i></a></center>');
-            
-
-            if(data['barcode']!=""){
-                $('td', row).eq(5).html('<center><a href="<?php echo base_url();?>Administration/printBarcode/?id='+data['event_id']+'&barcode='+data['barcode']+'&name='+data['event_name']+'"><img src="<?php echo base_url(); ?>barcode/'+data['barcode']+'.jpg" width="50px" class="imgSmile"></a></center>');
-            }
-            else{
-            $('td', row).eq(5).html('<center>  <a target ="_blank"  href="<?php echo base_url();?>Administration/generate_barcode/'+data['event_id']+'"><i class="fa  fa-file iconFontSize-medium" ></i></a></center>');
-            }
-
-            //console.log(data['event_id']);
-          },
-
-          "columns": [
-              { "data": "event_name", "orderable": true },
-              { "data": "event_name", "orderable": true },
-              { "data": "event_desc", "orderable": true },
-              {"data": "event_image",
-              "render": function (data, type, full, meta) {
-                  return "<img src=\"<?php echo base_url() ?>assets/uploads/events/"+ data + "\" height=\"30\"  width=\"50\"/>";
-              }},
-              { "data": "event_desc", "orderable": false },
-               { "data": "event_id", "orderable": false },
-            ]
-    });
-    function confirmDelete(event_id){
-      var conf = confirm("Do you want to delete Event ?");
-      if(conf){
-        $.ajax({
-          url:"<?php echo base_url();?>Administration/deleteEvent/",
-          data:{event_id:event_id},
-          method:"POST",
-          datatype:"json",
-          success:function(data){
-            var options = $.parseJSON(data);
-            noty(options);
-            $table.ajax.reload();
-          }
-        });
-      }
+
+$table = $('#event').DataTable( {
+  "processing": true,
+  "serverSide": true,
+  "bDestroy" : true,
+  "ajax": {
+    "url": "<?php echo base_url();?>Administration/getEvent/",
+    "type": "POST",
+    "data" : function (data) {
+
     }
-
+  },
+  "createdRow": function ( row, data, index ){
+    console.log(data['event_qr']);
+    $table.column(0).nodes().each(function(node,index,dt){
+      $table.cell(node).data(index+1);
+    });
+    $('td', row).eq(4).html('<center><a href="<?php echo base_url();?>Administration/editEvent/'+data['event_id']+'"><i class="fa fa-edit iconFontSize-medium" ></i></a>&nbsp;&nbsp;&nbsp;<a onclick="return confirmDelete('+data['event_id']+')"><i class="fa fa-trash-o iconFontSize-medium" ></i></a></center>');
+
+
+    if(data['barcode']!=""){
+      $('td', row).eq(5).html('<center><a href="<?php echo base_url();?>Administration/printBarcode/?id='+data['event_id']+'&barcode='+data['barcode']+'&name='+data['event_name']+'"><img src="<?php echo base_url(); ?>barcode/'+data['barcode']+'.jpg" width="50px" class="imgSmile"></a></center>');
+    }
+    else{
+      $('td', row).eq(5).html('<center>  <a target ="_blank"  href="<?php echo base_url();?>Administration/generate_barcode/'+data['event_id']+'"><i class="fa  fa-file iconFontSize-medium" ></i></a></center>');
+    }
+
+    //console.log(data['event_id']);
+  },
+
+  "columns": [
+    { "data": "event_name", "orderable": true },
+    { "data": "event_name", "orderable": true },
+    { "data": "event_desc", "orderable": true },
+    {"data": "event_image",
+    "render": function (data, type, full, meta) {
+      return "<img src=\"<?php echo base_url() ?>assets/uploads/events/"+ data + "\" height=\"30\"  width=\"50\"/>";
+    }},
+    { "data": "event_desc", "orderable": false },
+    { "data": "event_id", "orderable": false },
+    // {"data": "event_qr",
+    // "render": function (data, type, full, meta) {
+    //   return "<img src='data:image/png;base64,"+data['event_qr']+" height=30 width=50 />";
+    //
+    // }},
+    { "data": "event_qr", render : function (data, type, full, meta)
+    { return '<img src="data:image/png;base64,'+data+'"  height=\"30\"  width=\"50\"/>'; }},
+
+  ]
+});
+function confirmDelete(event_id){
+  var conf = confirm("Do you want to delete Event ?");
+  if(conf){
+    $.ajax({
+      url:"<?php echo base_url();?>Administration/deleteEvent/",
+      data:{event_id:event_id},
+      method:"POST",
+      datatype:"json",
+      success:function(data){
+        var options = $.parseJSON(data);
+        noty(options);
+        $table.ajax.reload();
+      }
+    });
+  }
+}
+
 </script>
